320a4059be8087705f0806a23843ee7c64f3c678
Gavin Dean <gavin@gavindeanit.com>
2017-11-29 11:32:09 -0800

    Initial commit
    Undocumented code

---
+++ catFacts.ps1
@@ -0,0 +1,10 @@
+﻿$AllProtocols = [System.Net.SecurityProtocolType]'Ssl3,Tls,Tls11,Tls12'
+[System.Net.ServicePointManager]::SecurityProtocol = $AllProtocols
+$webReply = Invoke-WebRequest -Uri "https://catfact.ninja/fact" -UseBasicParsing
+$catJson = $webReply.Content
+$catObject = ConvertFrom-Json $catJson
+#$wshell = New-Object -ComObject Wscript.Shell
+Add-Type -AssemblyName System.speech 
+$speak = New-Object System.Speech.Synthesis.SpeechSynthesizer 
+$speak.Speak($catObject.fact)
+#$wshell.Popup($catObject.facts,0,"Cats",0x1)
