61a808ee5dc26acf0d2d52ab0bc7a0590b9b7433
gavindeanit <32588673+gavindeanit@users.noreply.github.com>
2017-12-01 16:39:22 -0800

    Shortened code
    Thanks Reddit!

--- catFacts.ps1
+++ catFacts.ps1
@@ -1,10 +1,4 @@
-﻿$AllProtocols = [System.Net.SecurityProtocolType]'Ssl3,Tls,Tls11,Tls12'
-[System.Net.ServicePointManager]::SecurityProtocol = $AllProtocols
-$webReply = Invoke-WebRequest -Uri "https://catfact.ninja/fact" -UseBasicParsing
-$catJson = $webReply.Content
-$catObject = ConvertFrom-Json $catJson
-#$wshell = New-Object -ComObject Wscript.Shell
-Add-Type -AssemblyName System.speech 
-$speak = New-Object System.Speech.Synthesis.SpeechSynthesizer 
-$speak.Speak($catObject.fact)
-#$wshell.Popup($catObject.fact,0,"Cats",0x1)
+#Creates the object
+Add-Type -AssemblyName System.Speech;
+#Runs the code
+$(New-object "System.Speech.Synthesis.SpeechSynthesizer").speak($(irm 'https://catfact.ninja/fact').fact)
