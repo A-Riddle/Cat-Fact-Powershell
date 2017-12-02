5cb01533182a5a5fbbd323b6954e80517ce35278
gavindeanit <32588673+gavindeanit@users.noreply.github.com>
2017-12-01 16:59:34 -0800

    Ran into a security
    Ran into a security

--- catFacts.ps1
+++ catFacts.ps1
@@ -1,4 +1,6 @@
+$AllProtocols = [System.Net.SecurityProtocolType]'Ssl3,Tls,Tls11,Tls12'
+[System.Net.ServicePointManager]::SecurityProtocol = $AllProtocols
 #Creates the object
 Add-Type -AssemblyName System.Speech;
 #Runs the code
-$(New-object "System.Speech.Synthesis.SpeechSynthesizer").speak($(irm 'https://catfact.ninja/fact').fact)
+$(New-object "System.Speech.Synthesis.SpeechSynthesizer").speak($(irm 'https://catfact.ninja/fact' -UseBasicParsing).fact)
